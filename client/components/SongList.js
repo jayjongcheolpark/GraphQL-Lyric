1b474b5263333298c25c414cb50040cbceb89b9d
gogolearn <jay.jongcheol.park@gmail.com>
2017-11-21 23:10:43 -0500

    Add SongList component

---
+++ client/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
+  render () {
+    return (
+      <ul className="collection">
+        {this.props.data.loading || this.renderSongs()}
+      </ul>
+    )
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id,
+      title
+    }
+  }
+`
+
+
+export default graphql(query)(SongList)
