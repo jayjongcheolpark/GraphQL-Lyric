bb0c215ea7e2836da8a30da74edfc38db2afb73e
gogolearn <jay.jongcheol.park@gmail.com>
2017-11-22 18:06:32 -0500

    Navigating Between Screens

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 import fetchSong from '../queries/fetchSong'
 
@@ -8,6 +9,7 @@ class SongDetail extends Component {
     const { data } = this.props
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>{data.loading || data.song.title}</h3>
       </div>
     )
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -22,7 +22,9 @@ class SongList extends Component {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <Li key={id} className="collection-item">
-          {title}
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
           <i className="material-icons" onClick={() => this.onSongDelete(id)}>
             delete
           </i>
